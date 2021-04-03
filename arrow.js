563935dde7df1372916820f54b1f176d2bd0c0fc
saam1md <saam1mdalam@gmail.com>
2021-04-03 12:11:51 -0400

    es6 practice

---
+++ arrow.js
@@ -0,0 +1,11 @@
+function doubleIt(num) {
+    return num * 2;  
+}
+
+const result = doubleIt(5);
+console.log(result);
+
+const doubleIt1 = num => num *2;
+
+const Result = doubleIt1(50);
+console.log(Result);
